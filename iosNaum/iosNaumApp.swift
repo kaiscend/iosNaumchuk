b9fad91f7d26b2ad364dcfc12c983399e640026e
kaiscend <esnaum38@gmail.com>
2024-02-23 15:35:40 +0600

    Initial Commit

---
+++ iosNaum/iosNaumApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosNaumApp.swift
+//  iosNaum
+//
+//  Created by Nes on 23.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct iosNaumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
